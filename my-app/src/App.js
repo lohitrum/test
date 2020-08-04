3e87f08814cec955035def9af0596ae4f3fbc953
DAVID JESUS LIZARRAGA MENDOZA <1122723@utp.edu.pe>
2020-08-04 15:44:30 -0500

    primer cambio en github

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Primer cambio
         </a>
       </header>
     </div>
